bc26629e7ecbf6aecdb92e36d8e294156e18c991
hirokoymj <hiroko@hirokoymj.com>
2024-03-18 13:20:59 +0900

    added pdf link

--- src/Components/Layouts/Footer.jsx
+++ src/Components/Layouts/Footer.jsx
@@ -4,6 +4,7 @@ import Link from "@material-ui/core/Link";
 import IconButton from "@material-ui/core/IconButton";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import { makeStyles } from "@material-ui/core/styles";
+import resumePdf from "Documents/resume.pdf";
 
 const useStyles = makeStyles((theme) => ({
   footer: {
@@ -44,6 +45,11 @@ export const PageFooter = () => {
         icon={<GitHubIcon fontSize="large" />}
         to="https://github.com/hirokoymj/hiroko-frontend"
       />
+      <div>
+        <a href={resumePdf} target="_blank">
+          Resume
+        </a>
+      </div>
     </footer>
   );
 };
